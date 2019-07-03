1b0c73d9e5dc2fe44aa3338795cbfd9e2deb0318
Glitch (hello-express) <none>
2019-07-03 05:49:17 +0000

    🐋✨ Checkpoint ./server.js:3443349/63 ./tsconfig.json:3443349/115 ./package.json:3443349/66 ./server.ts:3443349/44

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-require('express')().use((r,w)=>w.send('')).listen(8080)
-
-
+require("express");
+express().use(function (r, w) { return w.send(r); }).listen(8080);
 // function method1(){
-  
-  
 // }
 // module.exports = method1
-
 // // file 2
-
-// test = require('./file1').method1()
+// test = require('./file1').method1() 
---
+++ server.ts
@@ -0,0 +1,15 @@
+import "express";
+
+express().use((r,w)=>w.send(r)).listen(8080);
+
+
+
+// function method1(){
+  
+  
+// }
+// module.exports = method1
+
+// // file 2
+
+// test = require('./file1').method1()
